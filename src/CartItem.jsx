d0b8b728408f7e3ed80842d3d4066a25d6babb89
Balarathi-P <balarathip07@gmail.com>
2025-02-11 12:52:43 -0500

    Still two more steps left to go

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,58 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+
+    return cart.reduce((total, item) => {
+
+        return total + item.quantity * item.cost;
+    },0);
+
  
   };
 
   const handleContinueShopping = (e) => {
+
+    if(onContinueShopping){
+
+        onContinueShopping();
+    }
    
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+
+    dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }));
+    updateCartIcon();
+
+
   };
 
   const handleDecrement = (item) => {
+
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.id));
+    }
    
   };
 
   const handleRemove = (item) => {
+
+    dispatch(removeItem(item.id));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+
+    return item.quantity * item.cost;
+
   };
 
   return (
